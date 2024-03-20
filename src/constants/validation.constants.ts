d497dd357e90503afc2e81a8dbbcbce838b02789
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-03-20 11:23:04 +0800

    chore: update constant variable name

--- src/constants/validation.constants.ts
+++ src/constants/validation.constants.ts
@@ -1,1 +1,6 @@
-export const address_permitted_special_characters_message = ". , ' : ; ( ) ° @ # / -";
+/**
+ * Represents the special characters permitted in an address.
+ * @remarks This is to be used in the message of the validation error, to let the user know which characters are permitted.
+ * @example `Special characters permitted: ${ValidationConstants.ADDRESS_PERMITTED_SPECIAL_CHARACTERS}`
+ */
+export const ADDRESS_PERMITTED_SPECIAL_CHARACTERS = ". , ' : ; ( ) ° @ # / -";
