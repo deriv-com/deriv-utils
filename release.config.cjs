74afc36d86a70dcb9b382c9ee1c01341f02bd54c
adrienne-rio <adrienne@deriv.com>
2024-04-04 11:04:33 +0800

    chore: update main branch

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,7 @@ const transform = require("./release.utils.cjs");
 module.exports = {
     branches: [
         "+([0-9])?(.{+([0-9]),x}).x",
-        "main",
+        "master",
         "next",
         "next-major",
         {
