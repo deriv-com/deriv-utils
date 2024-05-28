6cf490f156a763361370ceca776d713b94fe634c
yashim-deriv <yashim@regentmarkets.com>
2024-05-28 11:28:35 +0800

    chore: fix promise util types

--- src/utils/promise.utils.ts
+++ src/utils/promise.utils.ts
@@ -4,16 +4,17 @@
  * outside of the promise constructor callback.
  *
  * @template T The type of the value with which the promise will be resolved.
+ * @template U The type of the reason with which the promise will be rejected.
  * @returns An object containing:
  * - `promise`: A Promise<T> that can be awaited or otherwise used.
  * - `resolve`: A function that, when called with a value of type T, resolves the promise.
- * - `reject`: A function that, when called with a reason, rejects the promise.
+ * - `reject`: A function that, when called with a reason of type U (defaults to unknown), rejects the promise.
  */
-export const createPromise = <T>() => {
-    let deferredResolve!: (value?: T | null) => void;
-    let deferredReject!: (reason?: unknown) => void;
+export const createPromise = <T, U = unknown>() => {
+    let deferredResolve!: (value: T) => void;
+    let deferredReject!: (reason: U) => void;
 
-    const promise = new Promise<T | undefined | null>((resolve, reject) => {
+    const promise = new Promise<T>((resolve, reject) => {
         deferredResolve = resolve;
         deferredReject = reject;
     });
